0c56323700bdb342ceada5291f36a118b5f82742
Ebenezer Eshun <softhackghana2010@gmail.com>
2014-09-13 23:49:50 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial: Twitter sample application
+
+This is a sample application for [*Ruby on Rails Tutorials: Learn by Example*] (http://railstutorial.org/) by [Michael Hartl] (http://michaelhartl.com).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'df83874ebaebd3f63a678f10e8eb73bdaba2c7275a0df8c06a13634b70a0f826d4def360f9ef78aa20896d026bd00735b19bbb73d22f8f9eb70f93b78fc297f9'
